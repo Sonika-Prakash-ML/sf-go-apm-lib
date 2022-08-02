c9cfb56cca12e54bd35306608c2c8c421f35db7f
Sonika Prakash <89075668+Sonika-dot-Prakash@users.noreply.github.com>
2022-08-02 18:16:03 +0530

    added the Init() function

--- snappyflow.go
+++ snappyflow.go
@@ -38,7 +38,7 @@ func InitDefault() {
 	if err == nil && host != nil {
 		osType = host.OS
 	} else {
-		fmt.Println("Error while getting the host info:", err)
+		// fmt.Println("Error while getting the host info:", err)
 		os.Exit(1)
 	}
 	if osType == "windows" {
@@ -48,15 +48,15 @@ func InitDefault() {
 	}
 	config, configerr = LoadConfigFromFile(configPath) // gets only profile key and tags
 	if configerr != nil {
-		fmt.Println("Error while reading config file:", configerr)
+		// fmt.Println("Error while reading config file:", configerr)
 		os.Exit(1)
 	}
-	fmt.Println("profile key:", config.SnappyFlowKey)
-	fmt.Println("tags:", config.Tags)
+	// fmt.Println("profile key:", config.SnappyFlowKey)
+	// fmt.Println("tags:", config.Tags)
 
 	traceServerURL, profileID, err := getProfileData(config.SnappyFlowKey)
 	if err != nil {
-		fmt.Println("Error while decrypting key:", err)
+		// fmt.Println("Error while decrypting key:", err)
 		os.Exit(1)
 	}
 	err = setEnvVariables(traceServerURL, profileID, config.Tags)
@@ -65,44 +65,83 @@ func InitDefault() {
 	}
 }
 
+// Init sets the values from environment variables
+func Init(sfKey string, projectName string, appName string) {
+	// sfKey, projectName, appName := getFromEnvVariables()
+	tags := &Tags{
+		ProjectName: projectName,
+		AppName:     appName,
+	}
+	// fmt.Println("profile key:", sfKey)
+	// fmt.Println("tags:", tags)
+
+	traceServerURL, profileID, err := getProfileData(sfKey)
+	if err != nil {
+		// fmt.Println("Error while decrypting key:", err)
+		os.Exit(1)
+	}
+	err = setEnvVariables(traceServerURL, profileID, *tags)
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
 func getProfileData(key string) (string, string, error) {
 	decryptedKey, err := base64.StdEncoding.DecodeString(EncryptedKey)
 	if err != nil {
-		fmt.Println("Unable to decrypyt EncryptedKey:", err)
+		// fmt.Println("Unable to decrypyt EncryptedKey:", err)
 		return "", "", err
 	}
-	fmt.Println("decryptedKey:", string(decryptedKey))
+	// fmt.Println("decryptedKey:", string(decryptedKey))
 	data, err := decryptKey(key, decryptedKey)
 	if err != nil {
-		fmt.Println("unable to decrypt key:", err)
+		// fmt.Println("unable to decrypt key:", err)
 		return "", "", err
 	}
 	var keydata SnappyFlowKeyData
 	err = json.Unmarshal([]byte(data), &keydata)
 	if err != nil {
-		fmt.Println("unable to unmarshal key data:", err)
+		// fmt.Println("unable to unmarshal key data:", err)
 		return "", "", err
 	}
-	fmt.Println("trace server url:", keydata.TraceServer)
-	fmt.Println("profile id:", keydata.ProfileID)
+	// fmt.Println("trace server url:", keydata.TraceServer)
+	// fmt.Println("profile id:", keydata.ProfileID)
 	return keydata.TraceServer, keydata.ProfileID, nil
 }
 
 func setEnvVariables(traceURL string, profileID string, tags Tags) error {
 	globalLabels := fmt.Sprintf(GlobalLabels, tags[ProjectName], tags[AppName], profileID)
-	fmt.Println("global labels:", globalLabels)
+	// fmt.Println("global labels:", globalLabels)
 
-	err := os.Setenv("TEST_APM_SERVER_URL", traceURL)
-	err = os.Setenv("TEST_APM_GLOBAL_LABELS", globalLabels)
+	// err := os.Setenv("TEST_APM_SERVER_URL", traceURL)
+	// err = os.Setenv("TEST_APM_GLOBAL_LABELS", globalLabels)
+	err := os.Setenv(ElasticAPMServerURL, traceURL)
+	err = os.Setenv(ElasticAPMGlobalLabels, globalLabels)
 	if err != nil {
-		fmt.Println("error while setting environment variables:", err)
-		return err
 	}
-	fmt.Println("url env:", os.Getenv("TEST_APM_SERVER_URL"))
-	fmt.Println("tags env:", os.Getenv("TEST_APM_GLOBAL_LABELS"))
+
+	// _, cmdErr := exec.Command("cmd.exe", "/C", "setx", ElasticAPMServerURL, traceURL, "/m").Output()
+	// if cmdErr != nil {
+	// 	// fmt.Println("error while setting environment variables:", err)
+	// 	return cmdErr
+	// }
+	// _, cmdErr = exec.Command("cmd.exe", "/C", "setx", ElasticAPMGlobalLabels, globalLabels, "/m").Output()
+	// if cmdErr != nil {
+	// 	// fmt.Println("error while setting environment variables:", err)
+	// 	return cmdErr
+	// }
+	// fmt.Println("url env:", os.Getenv("TEST_APM_SERVER_URL"))
+	// fmt.Println("tags env:", os.Getenv("TEST_APM_GLOBAL_LABELS"))
 
 	return nil
 }
+
+// func getFromEnvVariables() (string, string, string) {
+// 	sfKey := os.Getenv(SfProfileKey)
+// 	projectName := os.Getenv(SfProjectName)
+// 	appName := os.Getenv(SfAppName)
+// 	return sfKey, projectName, appName
+// }
 
 func decryptKey(rawData string, key []byte) (string, error) {
 	data, err := base64.StdEncoding.DecodeString(rawData)
